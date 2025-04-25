c2f3477fcfd219cfb757b8302a2d4ba613d10db3
amphipath <amphipath@users.noreply.github.com>
2025-04-25 22:51:29 +0800

    add kms ign loader

--- resources/resources.go
+++ resources/resources.go
@@ -8,6 +8,13 @@ import (
 //go:embed igns.txt
 var igns []byte
 
+//go:embed kmsigns.txt
+var kmsIGNs []byte
+
 func LoadIGNs() []string {
 	return strings.Split(string(igns), "\r\n")
+}
+
+func LoadKMSIGNs() []string {
+	return strings.Split(string(kmsIGNs), "\r\n")
 }
